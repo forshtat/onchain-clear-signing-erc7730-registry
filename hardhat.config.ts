76ac5c4af6ea0f03869e4a0e3b1b8c795196cb09
Alex Forshtat <forshtat1@gmail.com>
2026-09-22 23:38:31 +0200

    Project scaffold

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  plugins: [hardhatToolboxViemPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.24",
+        settings: {
+          evmVersion: "cancun",
+        },
+      },
+      production: {
+        version: "0.8.24",
+        settings: {
+          evmVersion: "cancun",
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+  },
+});
